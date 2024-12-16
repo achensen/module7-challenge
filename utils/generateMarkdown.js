d639a92e656e45709cb285b9226d07ab50207b7e
achensen <Erinrjacobsen@gmail.com>
2024-12-16 09:19:56 -0500

    readme additinal notes added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -42,8 +42,9 @@ ${renderLicenseLink(data.license)}
 * [Installation](#installtion)
 * [Usage](#usage)
 * [Contribution](#contribution)
-* [Contact Information]
-* [Link to Walkthough Video]
+* [Contact Information](#contact information)
+* [Walkthough Video](#walkthrough video)
+* [Additional Notes](#additional notes)
 
 ## Description
 ${data.description}
@@ -55,10 +56,21 @@ ${data.installation}
 ## Usage
 ${data.usage}
 
+## Contributions
+${data.contribution}
+
 ## Contact Information
 My GitHub account is [GitHub Account Link](https://github.com/${data.github})
 To email me you can contact me at ${data.email}
 
+## Walkthrough Video
+Please follow this link for my Screencastify walkthrough
+
+## Additional Notes 
+I did recieve tutoring assistance to help with my unerstanding of this assignment. I was having an issue in VS code 
+where writeFile was not overwriting my previous ReadMe that I generated. My tutor helped me trouble shoot this issue 
+by showing me how to redownload my window in VS code, I also verbally walked through all the code to make sure my conceptual
+understanding was accuarate, and I got clarification on ternary operators like the one used in lines 74/75 of my index.js.
 `;
 }
 
